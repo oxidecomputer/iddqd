aebb7c55b0d368c6b1f0c08ab950134c72f5b1de
Rain <rain@oxide.computer>
2025-04-01 22:49:03 +0000

    add TriHashMap, import code from Omicron

--- src/tri_hash_map/mod.rs
+++ src/tri_hash_map/mod.rs
@@ -2,6 +2,4 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-mod tri_hash_map;
-
-pub use tri_hash_map::imp::{TriHashMap, TriHashMapEntry};
+pub(crate) mod imp;
