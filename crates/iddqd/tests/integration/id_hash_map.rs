325fcdb16c8b4d2314d885248e0c1f9fc03749c0
Rain <rain@oxide.computer>
2025-05-20 18:38:16 +0000

    remove duplicate test

--- crates/iddqd/tests/integration/id_hash_map.rs
+++ crates/iddqd/tests/integration/id_hash_map.rs
@@ -355,30 +355,6 @@ fn insert_panics_for_non_matching_key() {
 
 #[test]
 #[should_panic = "key hashes do not match"]
-fn insert_mut_panics_for_non_matching_key() {
-    let v1 = TestItem {
-        key1: 0,
-        key2: 'a',
-        key3: "foo".to_owned(),
-        value: "value".to_owned(),
-    };
-    let mut map = IdHashMap::new();
-    map.insert_unique(v1.clone()).expect("insert_unique succeeded");
-
-    let v2 = TestItem {
-        key1: 1,
-        key2: 'a',
-        key3: "bar".to_owned(),
-        value: "value".to_owned(),
-    };
-    let entry = map.entry(v2.key());
-    assert!(matches!(entry, Entry::Vacant(_)));
-    // Try inserting v1, which is present in the map.
-    entry.or_insert(v1);
-}
-
-#[test]
-#[should_panic = "key hashes do not match"]
 fn insert_entry_panics_for_non_matching_key() {
     let v1 = TestItem {
         key1: 0,
