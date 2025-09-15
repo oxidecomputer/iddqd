81d4674104b5fdd96afe2946caf652e9cbae0424
Rain <rain@oxide.computer>
2025-09-14 17:23:19 -0700

    [iddqd-benches] add benches for get functions (#150)
    iddqd is definitely a bit slower due to the multiple lookups required,
    but it's still quite competitive overall.
    
    ```
    hash_map_u32_get        time:   [29.941 ns 30.200 ns 30.568 ns]
    Found 8 outliers among 100 measurements (8.00%)
      2 (2.00%) low severe
      2 (2.00%) high mild
      4 (4.00%) high severe
    
    id_hash_map_owned_u32_get
                            time:   [49.183 ns 49.419 ns 49.648 ns]
    Found 1 outliers among 100 measurements (1.00%)
      1 (1.00%) low severe
    
    id_hash_map_borrowed_u32_get
                            time:   [46.885 ns 46.914 ns 46.941 ns]
    Found 7 outliers among 100 measurements (7.00%)
      2 (2.00%) low severe
      2 (2.00%) low mild
      2 (2.00%) high mild
      1 (1.00%) high severe
    
    btree_map_u32_get       time:   [36.684 ns 37.176 ns 37.662 ns]
    Found 8 outliers among 100 measurements (8.00%)
      7 (7.00%) low severe
      1 (1.00%) high severe
    
    id_ord_map_owned_u32_get
                            time:   [51.760 ns 51.872 ns 51.987 ns]
    Found 2 outliers among 100 measurements (2.00%)
      2 (2.00%) low severe
    
    id_ord_map_borrowed_u32_get
                            time:   [50.047 ns 50.128 ns 50.203 ns]
    Found 17 outliers among 100 measurements (17.00%)
      6 (6.00%) low severe
      5 (5.00%) low mild
      3 (3.00%) high mild
      3 (3.00%) high severe
    ```

--- crates/iddqd-benches/src/lib.rs
+++ crates/iddqd-benches/src/lib.rs
@@ -1,1 +1,51 @@
+use iddqd::{IdHashItem, IdOrdItem, id_upcast};
 
+pub struct RecordOwnedU32 {
+    pub index: u32,
+    pub data: String,
+}
+
+impl IdHashItem for RecordOwnedU32 {
+    type Key<'a> = u32;
+
+    fn key(&self) -> Self::Key<'_> {
+        self.index
+    }
+
+    id_upcast!();
+}
+
+impl IdOrdItem for RecordOwnedU32 {
+    type Key<'a> = u32;
+
+    fn key(&self) -> Self::Key<'_> {
+        self.index
+    }
+
+    id_upcast!();
+}
+
+pub struct RecordBorrowedU32 {
+    pub index: u32,
+    pub data: String,
+}
+
+impl IdHashItem for RecordBorrowedU32 {
+    type Key<'a> = &'a u32;
+
+    fn key(&self) -> Self::Key<'_> {
+        &self.index
+    }
+
+    id_upcast!();
+}
+
+impl IdOrdItem for RecordBorrowedU32 {
+    type Key<'a> = &'a u32;
+
+    fn key(&self) -> Self::Key<'_> {
+        &self.index
+    }
+
+    id_upcast!();
+}
