af328c41e1f8589d7702d80ec2db6d029e24a1c4
Rain <rain@oxide.computer>
2025-09-15 00:12:13 +0000

    [spr] initial version
    Created using spr 1.3.6-beta.1

--- crates/iddqd-benches/benches/benches.rs
+++ crates/iddqd-benches/benches/benches.rs
@@ -4,8 +4,10 @@
 //! live here.
 
 use criterion::{Criterion, criterion_group, criterion_main};
-use iddqd::IdOrdMap;
+use iddqd::{DefaultHashBuilder, IdHashMap, IdOrdMap};
+use iddqd_benches::{RecordBorrowedU32, RecordOwnedU32};
 use iddqd_test_utils::test_item::{TestItem, TestKey1};
+use std::collections::{BTreeMap, HashMap};
 
 fn bench_fn(c: &mut Criterion) {
     // Benchmark the id_ord_map::RefMut implementation with a very simple hash
@@ -16,8 +18,7 @@ fn bench_fn(c: &mut Criterion) {
     c.bench_function("id_ord_map_ref_mut_simple", |b| {
         b.iter_batched(
             || {
-                //
-                // Create a new IdOrdMap instance
+                // Create a new IdOrdMap instance.
                 let mut map = IdOrdMap::new();
                 map.insert_overwrite(TestItem::new(1, 'a', "foo", "bar"));
                 map
@@ -26,6 +27,137 @@ fn bench_fn(c: &mut Criterion) {
                 let mut item = map.get_mut(&TestKey1::new(&1)).unwrap();
                 item.key2 = 'b';
                 drop(item);
+            },
+            criterion::BatchSize::SmallInput,
+        );
+    });
+
+    c.bench_function("hash_map_u32_get", |b| {
+        b.iter_batched(
+            || {
+                let mut map =
+                    HashMap::with_hasher(DefaultHashBuilder::default());
+                map.insert(
+                    0u32,
+                    RecordOwnedU32 { index: 0, data: "data".to_owned() },
+                );
+                map.insert(
+                    1u32,
+                    RecordOwnedU32 { index: 1, data: "data1".to_owned() },
+                );
+                map
+            },
+            |map| {
+                map.get(&0);
+            },
+            criterion::BatchSize::SmallInput,
+        );
+    });
+
+    c.bench_function("id_hash_map_owned_u32_get", |b| {
+        b.iter_batched(
+            || {
+                // Create a new IdHashMap instance.
+                let mut map = IdHashMap::new();
+                map.insert_overwrite(RecordOwnedU32 {
+                    index: 0,
+                    data: "data".to_owned(),
+                });
+                map.insert_overwrite(RecordOwnedU32 {
+                    index: 1,
+                    data: "data1".to_owned(),
+                });
+                map
+            },
+            |map| {
+                map.get(&0);
+            },
+            criterion::BatchSize::SmallInput,
+        );
+    });
+
+    c.bench_function("id_hash_map_borrowed_u32_get", |b| {
+        b.iter_batched(
+            || {
+                // Create a new IdHashMap instance.
+                let mut map = IdHashMap::new();
+                map.insert_overwrite(RecordBorrowedU32 {
+                    index: 0,
+                    data: "data".to_owned(),
+                });
+                map.insert_overwrite(RecordBorrowedU32 {
+                    index: 1,
+                    data: "data1".to_owned(),
+                });
+                map
+            },
+            |map| {
+                map.get(&0);
+            },
+            criterion::BatchSize::SmallInput,
+        );
+    });
+
+    c.bench_function("btree_map_u32_get", |b| {
+        b.iter_batched(
+            || {
+                let mut map = BTreeMap::new();
+                map.insert(
+                    0u32,
+                    RecordOwnedU32 { index: 0, data: "data".to_owned() },
+                );
+                map.insert(
+                    1u32,
+                    RecordOwnedU32 { index: 1, data: "data1".to_owned() },
+                );
+                map
+            },
+            |map| {
+                map.get(&0);
+            },
+            criterion::BatchSize::SmallInput,
+        );
+    });
+
+    c.bench_function("id_ord_map_owned_u32_get", |b| {
+        b.iter_batched(
+            || {
+                // Create a new IdHashMap instance.
+                let mut map = IdOrdMap::new();
+                map.insert_overwrite(RecordOwnedU32 {
+                    index: 0,
+                    data: "data".to_owned(),
+                });
+                map.insert_overwrite(RecordOwnedU32 {
+                    index: 1,
+                    data: "data1".to_owned(),
+                });
+                map
+            },
+            |map| {
+                map.get(&0);
+            },
+            criterion::BatchSize::SmallInput,
+        );
+    });
+
+    c.bench_function("id_ord_map_borrowed_u32_get", |b| {
+        b.iter_batched(
+            || {
+                // Create a new IdHashMap instance.
+                let mut map = IdOrdMap::new();
+                map.insert_overwrite(RecordBorrowedU32 {
+                    index: 0,
+                    data: "data".to_owned(),
+                });
+                map.insert_overwrite(RecordBorrowedU32 {
+                    index: 1,
+                    data: "data1".to_owned(),
+                });
+                map
+            },
+            |map| {
+                map.get(&0);
             },
             criterion::BatchSize::SmallInput,
         );
