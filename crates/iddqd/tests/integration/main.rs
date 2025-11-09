0819d1e3c4e4cbc9878c985af0e6f53822473a26
Rain <rain@oxide.computer>
2025-11-08 16:48:46 -0800

    add snapshot test to measure sizes (#212)
    We're going to reduce the sizes of some of these maps in an upcoming PR.

--- crates/iddqd/tests/integration/main.rs
+++ crates/iddqd/tests/integration/main.rs
@@ -4,4 +4,11 @@ mod id_hash_map;
 mod id_ord_map;
 #[cfg(feature = "schemars08")]
 mod schemars_tests;
+#[cfg(all(
+    feature = "std",
+    feature = "default-hasher",
+    target_pointer_width = "64",
+    not(miri)
+))]
+mod size_tests;
 mod tri_hash_map;
